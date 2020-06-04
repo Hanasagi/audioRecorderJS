b7625cea849a4fa4363ffe3ff8cd3aeafe665cf3
Hanasagi <xenergymods@gmail.com>
2020-06-04 15:16:14 +0200

    Ajout attribut timer permettant de crée un timeout

--- lecture.js
+++ lecture.js
@@ -1,8 +1,33 @@
 
+let timerList;
+
+function log(message){
+	console.log(message);
+}
+
+
 function readAudio(id,filePath){
 	let reader = document.createElement("audio");
 	reader.src=filePath;
 	reader.controls="controls";
 	let idElement = document.getElementById(id);
 	idElement.appendChild(reader);
+}
+
+function getTimer(element){
+	timerList=element.querySelectorAll("[timer]");
+	nextElem(0);
+
+	}
+
+function nextElem(c){
+	if(c<timerList.length){
+	let seconds=timerList[c].getAttribute("timer");
+	log(seconds);
+	setTimeout(function(){
+		timerList[c].innerText="test";
+		c++;
+		next(c);
+	},seconds*1000);
+}
 }
